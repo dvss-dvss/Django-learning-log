519f0b61a3b93515959125a67c8ef7f340d84de3
dvss-dvss <kosevojdavid3@gmail.com>
2025-06-21 16:03:03 +0200

    Add registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 
+from . import views
+
 app_name = 'accounts'
 urlpatterns = [
     # Додати URL авторизації за замовчуванням
     path('', include('django.contrib.auth.urls')),
+    # Сторінка реєстрації
+    path('register/', views.register, name='register'),
 ]
 
